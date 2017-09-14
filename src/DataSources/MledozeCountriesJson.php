b169e6ed7806b53e81eb0070aaee56a50ea4176e
Jordan Hall <jordan.hall@rapidweb.biz>
2017-09-14 12:30:10 +0100

    Fix path

--- src/DataSources/MledozeCountriesJson.php
+++ src/DataSources/MledozeCountriesJson.php
@@ -13,7 +13,7 @@ class MledozeCountriesJson implements DataSourceInterface
     public function __construct()
     {
         $paths = [];
-        $paths[] = __DIR__.'/../../../mledoze/countries/dist/countries.json';
+        $paths[] = __DIR__.'/../../../../mledoze/countries/dist/countries.json';
         $paths[] = __DIR__.'/../../vendor/mledoze/countries/dist/countries.json';
 
         if (!$this->countryData) {
