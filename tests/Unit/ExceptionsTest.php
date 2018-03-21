48cf3413805e61ea9a734dcce40a9c4b784f282b
Jordan Hall <jordan.hall@rapidweb.biz>
2018-03-21 11:17:23 +0000

    Add exception test

---
+++ tests/Unit/ExceptionsTest.php
@@ -0,0 +1,26 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use RapidWeb\Countries\Countries;
+use RapidWeb\Countries\Country;
+use Exception;
+
+final class ExceptionsTest extends TestCase
+{
+    public function testRetrievingAllCountriesWithNoDataFile()
+    {
+        $dataFilePath = __DIR__.'/../../vendor/mledoze/countries/dist/countries.json';
+
+        if (file_exists($dataFilePath)) {
+            rename($dataFilePath, $dataFilePath.'_tmp');
+        }
+
+        $this->expectException(Exception::class);
+        $countries = (new Countries())->all();
+
+        if (file_exists($dataFilePath.'_tmp')) {
+            rename($dataFilePath.'_tmp', $dataFilePath);
+        }
+    }
+
+}
