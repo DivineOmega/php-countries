3e5493b9496b0d5d0eb83a0d184fd877fde42618
Jordan Hall <jordan.hall@rapidweb.biz>
2018-03-21 11:34:56 +0000

    Test of non-existant country retrieval

--- tests/Unit/BasicUsageTest.php
+++ tests/Unit/BasicUsageTest.php
@@ -25,6 +25,13 @@ final class BasicUsageTest extends TestCase
         $this->assertEquals('GBR', $country->isoCodeAlpha3);
     }
 
+    public function testGetNonExistantCountryByName()
+    {
+        $country = (new Countries())->getByName('Unified Kingdom of Jordania');
+
+        $this->assertEquals(null, $country);
+    }
+
     public function testGetCountryByIsoCode3Char()
     {
         $country = (new Countries())->getByIsoCode('USA');
@@ -39,6 +46,13 @@ final class BasicUsageTest extends TestCase
 
         $this->assertEquals(Country::class, get_class($country));
         $this->assertEquals('United States', $country->name);
+    }
+
+    public function testGetNonExistantCountryByIsoCode()
+    {
+        $country = (new Countries())->getByIsoCode('UKJ');
+
+        $this->assertEquals(null, $country);
     }
 
     public function testGetCountriesByLanguages()
