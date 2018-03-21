a3ab8228671a6248a35a302cf05975728ab4f0d5
Jordan Hall <jordan.hall@rapidweb.biz>
2018-03-21 11:31:17 +0000

    Missing data file test

---
+++ tests/Unit/MissingDataFileTest.php
@@ -0,0 +1,30 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use RapidWeb\Countries\Countries;
+
+final class MissingDataFileTest extends TestCase
+{
+    private $dataFilePath = __DIR__.'/../../vendor/mledoze/countries/dist/countries.json';
+
+    protected function setUp()
+    {
+        if (file_exists($this->dataFilePath)) {
+            rename($this->dataFilePath, $this->dataFilePath.'_tmp');
+        }
+    }
+
+    protected function tearDown()
+    {
+        if (file_exists($this->dataFilePath.'_tmp')) {
+            rename($this->dataFilePath.'_tmp', $this->dataFilePath);
+        }
+    }
+
+    public function testRetrievingAllCountriesWithNoDataFile()
+    {
+        $this->expectException(\Exception::class);
+        $countries = (new Countries())->all();
+    }
+
+}
